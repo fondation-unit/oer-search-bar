c83f0b98f513958f1a74d96659177d92ccba1c7e
Hipjea <pierre.duverneix@gmail.com>
2022-10-13 17:21:43 +0200

    sass

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -5,12 +5,12 @@ import { OER_SEARCH_BAR_PLACEHOLDER, OER_SEARCH_BAR_TITLE, OER_SEARCH_BAR_URL } 
 
 const Searchbar = () => {
     return (
-        <div className="oer-searchbar">
-            <h3>{ OER_SEARCH_BAR_TITLE }</h3>
-            <div className="search-wrapper research" data-external-search-url={ OER_SEARCH_BAR_URL }>
+        <div className="oer-searchbar container">
+            <h3 className="oer-searchbar-title">{ OER_SEARCH_BAR_TITLE }</h3>
+            <div className="oer-searchbar-research" data-external-search-url={ OER_SEARCH_BAR_URL }>
                 <div className="input-group">
                     <input className="search-navbar form-control" value="" type="text" placeholder={ OER_SEARCH_BAR_PLACEHOLDER } />
-                    <button className="launch-search-btn btn btn-primary" type="button">
+                    <button className="launch-search-btn" type="button">
                         <i className="fas fa-search" />
                     </button>
                 </div>
